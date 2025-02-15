30f52c23b424a4b329ff198895f6cda81e943b6e
Thiago Macêdo <thiago95macedo@gmail.com>
2025-02-15 15:06:25 -0400

    FIX: Odoo< para WETH<

--- addons/test_mail/data/test_mail_data.py
+++ addons/test_mail/data/test_mail_data.py
@@ -980,7 +980,7 @@ aa.com" style=3D"text-decoration:none; color: white;">info@aust-mfg.com</a>=
                       </tr>
                       <tr>
                         <td align=3D"center">
-                            Powered by <a href=3D"https://www.weth.com.br">Odo=
+                            <a href=3D"https://www.weth.com.br">Odo=
 o</a>.
                         </td>
                       </tr>
